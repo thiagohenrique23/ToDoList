a9d8763044a2b386d2c71f11ea8ca1c0c81e572d
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-10-18 09:37:20 -0400

    Backend ✓

---
+++ backend/src/Router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { response, request } = require('./app');
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddlewares')   
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id', tasksMiddleware.validateFieldTitle, tasksMiddleware.validateFieldStatus, tasksController.updateTask);
+
+module.exports = router;  
---
+++ backend/src/middlewares/tasksMiddlewares.js
@@ -0,0 +1,32 @@
+const validateFieldTitle = (request, response, next) => {
+    const { body } = request;
+
+    if (body.title === undefined) {
+       return response.status(400).json({ message: 'The field "title" is required ' })
+    }
+
+    if (body.title === '') {
+       return response.status(400).json({ message: 'title cannot be empyt ' })
+    }
+
+    next();
+};
+
+const validateFieldStatus = (request, response, next) => {
+    const { body } = request;
+
+    if (body.status === undefined) {
+       return response.status(400).json({ message: 'The field "status" is required ' })
+    }
+
+    if (body.status === '') {
+       return response.status(400).json({ message: 'status cannot be empyt ' })
+    }
+
+    next();
+};
+
+module.exports = {
+    validateFieldTitle,
+    validateFieldStatus,
+};
